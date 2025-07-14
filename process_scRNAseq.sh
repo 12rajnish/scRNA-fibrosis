6cc9278ae7c67cfd54da7e9690b839ebb6268646
Rajnish Kumar <65031996+12rajnish@users.noreply.github.com>
2025-07-14 15:27:36 -0500

    Update process_scRNAseq.sh

--- process_scRNAseq.sh
+++ process_scRNAseq.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Define input and output paths
 GTF_FILE="path_to/genomic.gtf"
 MITO_GENES_FILE="path_to/mitochondrial_genes.txt"
 R_SCRIPT="path_to/process_scRNAseq.R"
@@ -8,7 +7,7 @@ R_SCRIPT="path_to/process_scRNAseq.R"
 grep -i "mitochondrial" "$GTF_FILE" | awk -F 'gene_id "' '{print $2}' | awk -F '"' '{print $1}' | sort | uniq > "$MITO_GENES_FILE"
 
 if [ -s "$MITO_GENES_FILE" ]; then
-    echo "Mitochondrial gene extraction complete. Found $(wc -l < "$MITO_GENES_FILE") genes."
+    echo "extraction complete. Found $(wc -l < "$MITO_GENES_FILE") genes."
 else
     echo "Warning: No mitochondrial genes found. Check your GTF annotation file."
     exit 1
